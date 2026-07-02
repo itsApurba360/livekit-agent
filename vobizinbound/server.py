62b874387a440b03207cd5362251d2045919e315
itsApurba <itsapurba360@gmail.com>
2026-07-02 15:36:50 +0530

    feat: serve audio.wav and update Vobiz XML to play audio file

--- vobizinbound/server.py
+++ vobizinbound/server.py
@@ -4,14 +4,6 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 PORT = int(os.environ.get("PORT", "8000"))
 
-VOBIZ_XML = """<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Speak>Thank you for calling. Please call back to our company number eight nine five one two nine two seven eight eight.</Speak>
-  <Hangup/>
-</Response>
-"""
-
-
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self._handle()
@@ -23,7 +15,9 @@ class Handler(BaseHTTPRequestHandler):
         self._handle()
 
     def do_HEAD(self):
-        self._send_headers("application/xml; charset=utf-8", len(VOBIZ_XML))
+        host = self.headers.get("Host", "localhost")
+        xml_content = self._get_xml(host)
+        self._send_headers("application/xml; charset=utf-8", len(xml_content))
 
     def do_OPTIONS(self):
         self.send_response(200)
@@ -33,6 +27,13 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", "0")
         self.end_headers()
 
+    def _get_xml(self, host):
+        return f"""<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Play>https://{host}/audio.wav</Play>
+</Response>
+""".encode("utf-8")
+
     def _handle(self):
         if self.path == "/health":
             body = b"ok\n"
@@ -40,7 +41,23 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(body)
             return
 
-        body = VOBIZ_XML.encode("utf-8")
+        if self.path == "/audio.wav":
+            if os.path.exists("audio.wav"):
+                with open("audio.wav", "rb") as f:
+                    body = f.read()
+                self._send_headers("audio/wav", len(body))
+                self.wfile.write(body)
+            else:
+                body = b"Not Found"
+                self.send_response(404)
+                self.send_header("Content-Type", "text/plain")
+                self.send_header("Content-Length", str(len(body)))
+                self.end_headers()
+                self.wfile.write(body)
+            return
+
+        host = self.headers.get("Host", "localhost")
+        body = self._get_xml(host)
         self._send_headers("application/xml; charset=utf-8", len(body))
         self.wfile.write(body)
 
